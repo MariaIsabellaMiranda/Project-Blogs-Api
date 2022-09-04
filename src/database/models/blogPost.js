afdce614f1bbd3b2c445ccd35a4be939e332ab8d
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 16:16:25 -0300

    refactor:  [Model: blogPost]Retirado underscore e adicionado atributos do id.

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -2,14 +2,13 @@
 
 const BlogPost = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
-    id: DataTypes.STRING,
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
     published: DataTypes.DATE,
     updated: DataTypes.DATE
   }, {
-    underscored: true,
     timestamps: false,
     tableName: 'BlogPosts'
   });
