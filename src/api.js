1a765791150e018e2b6bd4ae1dd41b070f363eb0
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 17:20:40 -0300

    feat: [api.js] Criada nova rota para cadastramento de categorias no db

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
+const categoriesRouter = require('./routes/categoriesRouter');
 
 const app = express();
 app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
+app.use('/categories', categoriesRouter);
 
 module.exports = app;
