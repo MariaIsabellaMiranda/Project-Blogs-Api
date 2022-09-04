30548f02fcc6c365c9666324619be9facee137c7
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 16:28:59 -0300

    feat: [loginService] Criada função que implementa regras de negócio e realiza o login do usuário, retornando um novo token ao mesmo

---
+++ src/services/loginService.js
@@ -0,0 +1,18 @@
+const { User } = require('../database/models');
+const tokenHelper = require('../helpers/tokenHelper');
+
+const login = async (email, password) => {
+  if (!email || !password) return { code: 400, message: 'Some required fields are missing' };
+
+  const data = await User.findOne({ where: { email, password } });
+
+  if (!data) {
+    return { code: 400, message: 'Invalid fields' };
+  }
+
+  const token = tokenHelper.createToken(email);
+
+  return { code: 200, data: token };
+};
+
+module.exports = { login };
