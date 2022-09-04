5757de4ec64e07ba9144c1722771e2837d16120c
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 16:23:51 -0300

    feat: [userValidation.js] Criado arquivo para guardar módulos onde é feita a verificação das validações para criação de novos usuários e também módulo que verifica se o email já existe no db

---
+++ src/helpers/userValidation.js
@@ -0,0 +1,23 @@
+const schema = require('./userSchema');
+const { User } = require('../database/models');
+
+const userValidation = ({ displayName, email, password }) => {
+  const { error } = schema.userSchema.validate({ displayName, email, password });
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+
+    return { code, message };
+  }
+  return false;
+};
+
+const emailValidation = async ({ email }) => {
+  const verifyEmail = await User.findOne({ where: { email } });
+
+  if (verifyEmail) return { code: 409, message: 'User already registered' };
+
+  return false;
+};
+
+module.exports = { userValidation, emailValidation };
