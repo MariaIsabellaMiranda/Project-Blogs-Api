7f03b4aa1dea8bd56eeae89f45855af1baca32d3
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 16:11:07 -0300

    feat: [api.js] Adicionadas novas rotas. Rota para login e rota para user.

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-// ...
+const loginRouter = require('./routes/loginRouter');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
-
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
+app.use('/user', userRouter);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
