1f6b490f7bcf48051da15ffb4692d47405e4629a
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-05 00:26:52 -0300

    feat: [blogPostController.js] Criado controller para requisições onde busca o BlogPost através do id passado por params

--- src/controllers/blogPostController.js
+++ src/controllers/blogPostController.js
@@ -12,10 +12,20 @@ const createBlogPost = async (req, res) => {
   res.status(code).json(data);
 };
 
-const getBlogPostsAll = async (req, res) => {
+const getBlogPostsAll = async (_req, res) => {
   const { code, data } = await blogPostServices.getBlogPostsAll();
 
   res.status(code).json(data);
 };
 
-module.exports = { createBlogPost, getBlogPostsAll };
+const getBlogPostId = async (req, res) => {
+  const { id } = req.params;
+
+  const { code, data, message } = await blogPostServices.getBlogPostId(id);
+
+  if (!data) return res.status(code).json({ message });
+
+  res.status(code).json(data);
+};
+
+module.exports = { createBlogPost, getBlogPostsAll, getBlogPostId };
