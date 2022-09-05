f8b9ead4c8fc52d727d495a52090908c7e38a699
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 22:01:00 -0300

    feat: [api.js] Criada nova rota para requisições de cadastramento de blogPosts

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
 const categoriesRouter = require('./routes/categoriesRouter');
+const blogPostRouter = require('./routes/blogPostRouter');
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/categories', categoriesRouter);
+app.use('/post', blogPostRouter);
 
 module.exports = app;
