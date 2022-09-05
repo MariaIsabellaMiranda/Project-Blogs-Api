44ccd93c41b6be4c79a59b5480dd00ec6857a56d
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-05 00:28:07 -0300

    feat: [blogPostRouter.js] Criada rota para requisições de BlogPos através do seu id

--- src/routes/blogPostRouter.js
+++ src/routes/blogPostRouter.js
@@ -5,5 +5,6 @@ const blogPostController = require('../controllers/blogPostController');
 
 router.post('/', tokenAuth.validToken, rescue(blogPostController.createBlogPost));
 router.get('/', tokenAuth.validToken, rescue(blogPostController.getBlogPostsAll));
+router.get('/:id', tokenAuth.validToken, rescue(blogPostController.getBlogPostId));
 
 module.exports = router;
