5c956867453e361ab8d83b265ce48934f8534fab
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-04 22:02:07 -0300

    feat: [blogPostController.js] Criado controller para requisições para realizar o cadastro de novos BlogPosts

---
+++ src/controllers/blogPostController.js
@@ -0,0 +1,15 @@
+const blogPostServices = require('../services/blogPostServices');
+
+const createBlogPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { email } = req.user;
+
+  const { code, data, message } = await blogPostServices
+  .createBlogPost({ title, content, categoryIds }, email);
+
+  if (!data) return res.status(Number(code)).json({ message });
+
+  res.status(code).json(data);
+};
+
+module.exports = { createBlogPost };
